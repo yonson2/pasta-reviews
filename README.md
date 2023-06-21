50923cae8e90ceb1c45921718a0d68c0e406e08b
Peter <s@pramos.me>
2023-06-21 21:51:26 +0200

    feat: initial working site

---
+++ README.md
@@ -0,0 +1,8 @@
+# Reviews de pasta.
+
+This is a simple site built as a way of tracking all the possible pastas this new pasta place near our co-working has.
+
+# Contributing.
+
+ - To add a review, simply open a pull request modifying `pasta.json` including your review.
+ - Pull requests to improve the styling of the page are also welcomed, this site is currently styled with [pico](https://picocss.com) and served through [SvelteKit](https://kit.svelte.dev/)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+	import pasta from '$lib/pasta.json';
+	let [total, reviewed] = [0, 0];
+	console.log(pasta);
+	for (const [_, pastaTypeReviews] of Object.entries(pasta)) {
+		for (const [_, review] of Object.entries(pastaTypeReviews)) {
+			total++;
+			if (Object.keys(review).length !== 0) {
+				reviewed++;
+			}
+		}
+	}
+
+	function hasReviews(obj) {
+		for (const key in obj) {
+			if (Object.keys(obj[key]).length !== 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+</script>
+
+<header class="container">
+	<hgroup>
+		<h1>Pasta Balboa, revisada.</h1>
+		<h2>Reviews sobre las pastas de pasta balboa, por y para el amor a la pasta 🍝</h2>
+	</hgroup>
+</header>
+<main class="container">
+	<section id="reviews">
+		<h2>Reviews</h2>
+		{#each Object.entries(pasta) as [pastaType, pastaTypeReviews]}
+			{#if hasReviews(pastaTypeReviews)}
+				<section style="text-transform: capitalize;" id={pastaType}>
+					<h3>{pastaType}</h3>
+					{#each Object.entries(pastaTypeReviews) as [review_key, review_value]}
+						{#if Object.keys(review_value).length !== 0}
+							<article id={review_key}>
+								<h2>{review_value.title}</h2>
+								<p>{review_value.review}</p>
+								<footer>
+									<p>Valoración final:</p>
+									<progress value={review_value.score} max="100" min="0"/>
+									<center><small>{review_value.score} / 100</small></center>
+								</footer>
+							</article>
+						{/if}
+					{/each}
+				</section>
+			{/if}
+		{/each}
+	</section>
+	<section id="progress">
+		<h2>Progreso</h2>
+		<p>Se han probado {reviewed} de {total} posibles pastas!</p>
+		<progress value={reviewed} max={total} min="0"/>
+	</section>
+</main>
